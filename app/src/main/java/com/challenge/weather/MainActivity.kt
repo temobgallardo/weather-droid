973f865d510322e82847d026fa1913652c9f72ad
temobgallardo <62340961+temobgallardo@users.noreply.github.com>
2024-10-15 19:10:38 -0600

    first commit

---
+++ app/src/main/java/com/challenge/weather/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.challenge.weather
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.challenge.weather.ui.theme.WeatherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WeatherTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WeatherTheme {
+        Greeting("Android")
+    }
+}
+
+var weatherApiKey = "15c08d55dca35af18244780fa1a0f047"
+var weatherApiOp = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=$weatherApiKey"
