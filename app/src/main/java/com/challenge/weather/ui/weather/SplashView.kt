ba79abd17b528479e4ef56d70dfed6888aaaa6dc
temobgallardo <62340961+temobgallardo@users.noreply.github.com>
2024-10-25 09:05:58 -0600

    added gif video

--- app/src/main/java/com/challenge/weather/ui/weather/SplashView.kt
+++
@@ -1,8 +0,0 @@
-package com.challenge.weather.ui.weather
-
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Composer
-
-@Composable
-fun SplashView(composer: Composer) {
-}
